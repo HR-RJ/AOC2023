0c2778d9bafa97a20d69e0c74e0a6587dc32622d
System Administrator <root@MacBook-Pro-RJ.local>
2023-12-02 05:08:58 +0100

    completed day1 challenge 1

--- dec1/src/main.rs
+++ dec1/src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{self, BufRead};
+
+fn main() -> io::Result<()> {
+    let path = "input.txt";
+
+    let file = File::open(path)?;
+    let reader = io::BufReader::new(file);
+    let mut sum = 0;
+
+    for line in reader.lines() {
+        let line = line?;
+        // let mut i = 0;
+            // for c in line.chars() {
+            //     match c {
+            //         '0'..='9' => digits.push(vec![c]),
+            //         _ => ()
+
+            //     }
+            // }
+        println!("{:?}", line);
+
+        let mut left : i32 = -1;
+        let mut right : i32 = -1;
+
+        for i in line.chars() {
+            if i.is_digit(10){
+                if left == -1{
+                    left = i as i32 - '0' as i32;
+                    right = i as i32 - '0' as i32;
+                } else {
+                    right = i as i32 - '0' as i32;
+                }
+            }
+        }
+        println!("found: {:?},{:?}", left, right);
+        let totals = left * 10 + right;
+        println!("{:?}", totals);
+        sum += totals;
+    }
+    println!("{:?}", sum);
+    Ok(())
+
 }
